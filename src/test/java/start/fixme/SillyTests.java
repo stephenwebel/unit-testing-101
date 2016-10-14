7b9ede93ac97a2858a00f9b7cfca4d9a5232d9cb
stephenwebel <COLBY7101>
2016-10-14 03:10:32 -0400

    gitignore

---
+++ src/test/java/start/fixme/SillyTests.java
@@ -0,0 +1,45 @@
+package start.fixme;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ *
+ * Created by stephenwebel1 on 9/28/16.
+ */
+public class SillyTests {
+
+
+    @Test
+    public void assertingTrue(){
+        Assert.assertTrue("Why won't this pass?!?!", false);
+    }
+
+    @Test
+    public void assertingEquals(){
+        String expectedString = "the world";
+        String actualString = "The world";
+
+        Assert.assertEquals(expectedString,actualString);
+    }
+
+    @Test
+    public void assertingEqualPositions(){
+        Position expectedPosition = new Position(123.0,"IBM","webz123");
+        Position actualPosition = new Position(123.0,"IBM","webz123");
+
+        Assert.assertEquals("These objects should be equal... what happened?",expectedPosition,actualPosition);
+    }
+
+    @Test
+    public void assertingNotEqualTrades(){
+        TradeMutator tradeMutator = new TradeMutator();
+
+        Trade initialTrade = new Trade(123.0, Side.LONG, "IBM","webz123");
+        Trade mutatedTrade = tradeMutator.incrementTradeQty(initialTrade,100);
+
+        Assert.assertNotEquals("Wait.. wut?!?! The updated trade is still equal?",initialTrade,mutatedTrade);
+    }
+}
